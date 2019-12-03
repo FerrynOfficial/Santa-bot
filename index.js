2b5c38434ccca348395b54bafcfd1677611750dd
FerrynOfficial <47920832+FerrynOfficial@users.noreply.github.com>
2019-12-03 21:52:50 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+global.Discord = require('discord.js')
+const client = new Discord.Client();
+const fs = require('fs')
+client.login(process.env.BOT_TOKEN)
+
+client.owner = '525003205394825257';
+client.on('ready', () => {
+
+    client.user.setStatus('dnd')
+    client.user.setPresence({
+        game:{
+          name:`'FlameCommunity 2.0'| До нового года 4 недели!`,
+          type:"PLAYING"
+        }
+      }) 
+})
+client.prefix = 's!'
+client.commands = new Discord.Collection();
+fs.readdir('./cmds/', (err, files) => {
+    if(err) console.log(err)
+    let jsfiles = files.filter(f => f.split('.').pop() === 'js');
+    if(jsfiles.length <= 0) {
+        console.log('No commands!')
+        return
+    }
+    console.log(`[COMMANDS] Loaded ${files.length} commannds!`)
+    jsfiles.forEach(f => {
+        let props = require(`./cmds/${f}`)
+        client.commands.set(props.help.name, props)
+        
+    })
+
+    
+    })
+
+
+
+
+client.on('message', message => {
+    var msg = message.content.toLowerCase() || message.content.toUpperCase()
+    if(!msg.startsWith(client.prefix)) return;
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+    const args = message.content.slice(client.prefix.length).trim().split(/ +/g);
+let command = args.shift().toLowerCase();
+    let cmd;
+    if(client.commands.has(command)) {
+        cmd = client.commands.get(command)
+    }
+    if(!cmd) return;
+    cmd.run(client, message, args)
+})
